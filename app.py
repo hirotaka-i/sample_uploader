1a12e8f17b3a74018a94c475f4409cc3f3d9d6f4
Iwaki <iwakih2@NIALNG-02178851.fios-router.home>
2021-11-05 14:30:29 -0400

    debug

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def get_table_download_link(df):
 	csv = df.to_csv(index=False)
 	b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
 	href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
-    return href
+	return href
 	
 @st.cache
 def load_image(image_file):
