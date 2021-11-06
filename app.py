9228be65c808cf61c36d215b8131e6d04d856c91
Iwaki <iwakih2@NIALNG-02178851.fios-router.home>
2021-11-05 20:00:18 -0400

    v1m2

--- app.py
+++ app.py
@@ -57,8 +57,9 @@ def main():
 
 	st.title("GP2 sample manifest self-QC web app")
 	st.text('This is a web app to self-check the sample manifest')
-	st.text('The template download from the below link and go to "File"> "Download" as xlsx/csv')
+	st.text('The template download from the below link (go to "File"> "Download" as xlsx/csv)')
 	st.write('https://docs.google.com/spreadsheets/d/1ThpUVBCRaPdDSiQiKZVwFpwWbW8mZxhqurv7-jBPrM4')
+	st.text('Please refer to the second tab (data dictionary) for instruction')
 	st.text('')
 	if data_file is not None:
 		st.header("Data Check and self-QC")
